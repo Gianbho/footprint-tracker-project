597bf7d24c2ec851adb8a85f38f23e6a7fb0d2dd
Gianbho <gianluca.trm@gmail.com>
2022-06-14 14:28:50 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className='flex flex-col justify-center items-center h-[15%]'>
         <h1 className='font-mono text-3xl'>Flight footprint calculator</h1>
-        <h4 className='font-mono self-end'>Gianluca's React.js project</h4>
+        <h4 className='font-mono self-end'>Built with React.js</h4>
     </header>
   )
 }
